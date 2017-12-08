7d349d1ed1ffa8e4516e5c1a8b247c098b3c8205
Ryan Filax-Wylie <ryanfilaxwylie@gmail.com>
2017-12-08 07:30:13 -0700

    Grunt Part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,9 @@
 
 module.exports= function(grunt) {
 	require('time-grunt')(grunt);
-	require('jit-grunt')(grunt);
+	require('jit-grunt')(grunt,{
+		useminPrepare: 'grunt-usemin'
+	});
 	grunt.initConfig({
 		sass: {
 			dist:{
@@ -32,11 +34,114 @@ browserSync: {
 			}
 		}
 	}	
-}
+},
+copy:{
+	html: {
+		files:[{
+			expand: true,
+			dot: true,
+			cwd: './',
+			src: ['*.html'],
+			dest: 'dist'
+		}]
+	},
+	fonts:{
+		files: [{
+			expand: true,
+			dot: true,
+			cwd: 'node_modules/font-awesome',
+			src: ['fonts/*.*'],
+			dest: 'dist'
+		}]
+	}
+},
+clean: {
+	build:{
+		src:['dist/']
+	}
+},
+imagemin: {
+	dynamic:{
+		files:[{
+			expand: true,
+			cwd: './',
+			src:['img/*.{png, gif,jpg}'],
+			dest: 'dist/'
+		}]
+	}
+},
+	useminPrepare: {
+		foo:{
+			dest: 'dist',
+			src: ['contactus.html','aboutus.html','index.html']
+		},
+		options:{
+			flow: {
+				steps: {
+					css:['cssmin'],
+					js: ['uglify']
+				},
+				post:{
+					css:[{
+						name: 'cssmin',
+						createConfig: function(context,block){
+							var generated = context.options.generated;
+							generated.option={
+								keepSpecialComments:0,
+								rebase: false
+							};
+						}
+					}]
+				}
+			}
+		}
+	},
 
-	});
+	concat:{
+		options: {
+			seperator: ';'
+		},
+		dist:{}
+	},
+	uglify:{
+		dist:{}
+	},
+	cssmin:{
+		dist:{}
+	},
+	filerev: {
+		options:{
+			endcoding: 'utf8',
+			algorithm: 'md5',
+			lenght: 20
+		},
+		release:{
+			files: [{
+				src:['dist/css/*.css','dist/js/*.js']
+			}]
+		}
+	},
+	usemin:{
+		html: ['dist/contactus.html','dist/aboutus.html','dist/index.html'],
+		options:{
+			assetsDir: ['dist','dist/css','dist/js']
+		}
+	}
+
+});
 	grunt.registerTask('css',['sass']);
 	grunt.registerTask('default', ['browserSync', 'watch']);
+	grunt.registerTask('build',[
+		'clean',
+		'copy',
+		'imagemin',
+		'useminPrepare',
+		'concat',
+		'cssmin',
+		'uglify',
+		'filerev',
+		'usemin'
+		]);
 }
 
 
